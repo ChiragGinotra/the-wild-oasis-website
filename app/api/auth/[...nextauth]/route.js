7e4f635aa841cd051d10a69e4b7544a9d3337430
ChiragGinotra <Chirag0172.be20@chitkara.edu.in>
2025-02-18 01:04:35 +0530

    Complete Website v1

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,6 @@
+// import NextAuth from "next-auth";
+// import authConfig from "@/app/_lib/auth"; // Adjust based on your project structure
+
+// export const { GET, POST } = NextAuth(authConfig);
+
+export { GET, POST } from "@/app/_lib/auth";
